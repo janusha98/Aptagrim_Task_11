2d749c0c241a9f6bdfc098934d6a24d8850d8e9a
janusha98 <49071176+janusha98@users.noreply.github.com>
2021-07-06 06:12:36 -0700

    Final Code with bug fixes

--- src/Product.js
+++ src/Product.js
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Card, Row, Col, Form } from "react-bootstrap";
+import { Card, Row, Col, Form, Container } from "react-bootstrap";
 import axios from "axios";
 
 const Product = (props) => {
+  let allProducts = [
+    {
+      image: "/bag.jpg",
+      title: "Bag",
+      price: 80
+    },
+    {
+      image: "/shoe.jpg",
+      title: "Shoes",
+      price: 100
+    },
+    {
+      image: "/watch.jpg",
+      title: "Watch",
+      price: 180
+    }
+  ];
+
   const [currency, setCurrency] = useState({});
   const [currentConversionRate, setCurrentConversionRate] = useState(1);
-  let allProducts = [
-    { image: "holder.js/100px180", title: "Car", price: "80" },
-    { image: "holder.js/100px180", title: "bike", price: "100" },
-    { image: "holder.js/100px180", title: "honda", price: "180" }
-  ];
+
   const changeCurrencyType = (e) => {
     const currencyValue = e.target.value;
     if (currency.result === "success") {
@@ -19,6 +33,7 @@ const Product = (props) => {
       }
     }
   };
+
   useEffect(() => {
     axios
       .get(
@@ -27,31 +42,40 @@ const Product = (props) => {
       .then((response) => setCurrency(response.data))
       .catch((e) => console.log(e));
   }, []);
+
   return (
-    <Row>
-      <Col lg={9}>
-        {allProducts.map((product, id) => (
-          <Row key={id}>
-            <Card>
-              <Col lg={4}>
-                <Card.Img variant="top" src={product.image} />
-                <Card.Body>
-                  <Card.Title>{product.title}</Card.Title>
-                  <Card.Text>{product.price * currentConversionRate}</Card.Text>
-                </Card.Body>
+    <Container>
+      <Row>
+        <Col lg={8}>
+          <Row>
+            {allProducts.map((product, id) => (
+              <Col lg={4} key={id}>
+                <div>
+                  <Card>
+                    <Card.Img src={product.image} />
+                    <Card.Body>
+                      <Card.Title>{product.title}</Card.Title>
+                      <Card.Text>
+                        {(product.price * currentConversionRate).toFixed(2)}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
               </Col>
-            </Card>
+            ))}
           </Row>
-        ))}
-        ;
-      </Col>
-      <Col lg={3}>
-        <Form.Control as="select" onChange={changeCurrencyType}>
-          <option>INR</option>
-          <option>USD</option>
-        </Form.Control>
-      </Col>
-    </Row>
+        </Col>
+        <Col lg={4}>
+          <label>
+            <b>Currency</b>
+          </label>
+          <Form.Control as="select" onChange={changeCurrencyType}>
+            <option>INR</option>
+            <option>USD</option>
+          </Form.Control>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 export default Product;
