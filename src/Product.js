e9afa42b704f94b0978882b3680bffb7eb196f52
janusha98 <49071176+janusha98@users.noreply.github.com>
2021-07-06 00:29:08 -0700

    API token changes and use effect

--- src/Product.js
+++ src/Product.js
@@ -1,51 +1,57 @@
-import React,{useEffect,useState}  from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Card,Row,Col,Form} from "react-bootstrap";
+import { Card, Row, Col, Form } from "react-bootstrap";
 import axios from "axios";
 
 const Product = (props) => {
-  const [currency,setCurrency] = useState({});
+  const [currency, setCurrency] = useState({});
   const [currentConversionRate, setCurrentConversionRate] = useState(1);
-  let allProducts = [{image:"holder.js/100px180",title:"Car",price:"80"},
-    {image:"holder.js/100px180",title:"bike",price:"80"},
-    {image:"holder.js/100px180",title:"honda",price:"80"}
+  let allProducts = [
+    { image: "holder.js/100px180", title: "Car", price: "80" },
+    { image: "holder.js/100px180", title: "bike", price: "100" },
+    { image: "holder.js/100px180", title: "honda", price: "180" }
   ];
-  const changeCurrencyType =(e) => {
+  const changeCurrencyType = (e) => {
     const currencyValue = e.target.value;
-    if(currency.result == "success"){
-      if(currency.conversion_rates[currencyValue]){
-        setCurrentConversionRate(currency.conversion_rates[currencyValue])
+    if (currency.result === "success") {
+      if (currency.conversion_rates[currencyValue]) {
+        setCurrentConversionRate(currency.conversion_rates[currencyValue]);
       }
     }
   };
   useEffect(() => {
-    axios.get('https://v6.exchangerate-api.com/v6/a62668a95e5e4be06717ab30/latest/INR')
-    .then(response => setCurrency(response.data))
-  });
-   return(
-     allProducts.map((product,id) => 
-     <Row> 
-       <Col lg={9}>
-        <Card>
-        <Col lg={4}>
-        <Card.Img variant="top" src={product.image} />
-            <Card.Body>
-              <Card.Title>{product.title}</Card.Title>
-              <Card.Text>
-                {product.price*currentConversionRate}
-              </Card.Text>
-            </Card.Body>
-          </Col>
-      </Card>
+    axios
+      .get(
+        "https://v6.exchangerate-api.com/v6/7697dc6ab636cf10427c967c/latest/INR"
+      )
+      .then((response) => setCurrency(response.data))
+      .catch((e) => console.log(e));
+  }, []);
+  return (
+    <Row>
+      <Col lg={9}>
+        {allProducts.map((product, id) => (
+          <Row key={id}>
+            <Card>
+              <Col lg={4}>
+                <Card.Img variant="top" src={product.image} />
+                <Card.Body>
+                  <Card.Title>{product.title}</Card.Title>
+                  <Card.Text>{product.price * currentConversionRate}</Card.Text>
+                </Card.Body>
+              </Col>
+            </Card>
+          </Row>
+        ))}
+        ;
       </Col>
       <Col lg={3}>
-      <Form.Control as="select" onChange={changeCurrencyType}>
-        <option>INR</option>
-        <option>USD</option>
-      </Form.Control>
+        <Form.Control as="select" onChange={changeCurrencyType}>
+          <option>INR</option>
+          <option>USD</option>
+        </Form.Control>
       </Col>
-   </Row>
-  ) 
-  )
-}
+    </Row>
+  );
+};
 export default Product;
